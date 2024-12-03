31fa82f6ad2c399cfb79616c6a5e48699d367d7c
Jesse Nickles <jesse@littlebizzy.com>
2024-12-03 18:21:43 +0700

    Update disable-cart-fragments.php

--- disable-cart-fragments.php
+++ disable-cart-fragments.php
@@ -3,7 +3,7 @@
 Plugin Name: Disable Cart Fragments
 Plugin URI: https://www.littlebizzy.com/plugins/disable-cart-fragments
 Description: Disables AJAX fragments in Woo
-Version: 1.4.0
+Version: 2.0.0
 Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
 License: GPLv3
@@ -23,15 +23,36 @@ add_filter( 'gu_override_dot_org', function( $overrides ) {
 	return $overrides;
 }, 999 );
 
-// disable woocommerce cart fragments
+// disable woocommerce cart fragments and conditionally re-enable using a cookie check
 add_action( 'wp_enqueue_scripts', function() {
-	if ( class_exists( 'WooCommerce' ) ) {
-		// dequeue cart fragment scripts
-		wp_dequeue_script( 'wc-cart-fragments' );
-	}
-}, 11 );
+    if ( class_exists( 'WooCommerce' ) ) {
+        global $wp_scripts;
+
+        $handle = 'wc-cart-fragments';
+        if ( isset( $wp_scripts->registered[ $handle ] ) && $wp_scripts->registered[ $handle ] ) {
+            $load_cart_fragments_path = $wp_scripts->registered[ $handle ]->src;
+
+            // nullify the script source to prevent automatic loading
+            $wp_scripts->registered[ $handle ]->src = null;
+
+            // inject lightweight inline script to conditionally load cart fragments
+            wp_add_inline_script(
+                'jquery',
+                '
+                if ( document.cookie.indexOf("woocommerce_cart_hash") !== -1 ) {
+                    var script = document.createElement("script");
+                    script.src = "' . esc_url( $load_cart_fragments_path ) . '";
+                    script.async = true;
+                    document.head.appendChild(script);
+                }
+                '
+            );
+        }
+    }
+}, 999 );
 
 // Ref: ChatGPT
+// Ref: https://wordpress.org/plugins/disable-cart-fragments/
 // Ref: https://jeffmatson.net/stopping-cart-fragments-from-updating-in-woocommerce/
 // Ref: https://github.com/woocommerce/woocommerce/issues/9365
 // Ref: https://github.com/woocommerce/woocommerce/issues/7777
